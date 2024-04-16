6255c3e19fd66214ac6deb703c537be60f023365
Andile18 <zama.andile18@gmail.com>
2024-04-17 00:30:05 +0200

    Added AI API imigration

--- src/script.js
+++ src/script.js
@@ -1,18 +1,33 @@
-function generateAnswers(event) {
-    event.preventDefault();
- 
-
-new Typewriter('#financialAdvice', {
-    strings: ['What is financial literacy?'],
+function displayAnswers(response){
+ console.log (" advices generated"),
+    new Typewriter('#financialAdvice', {
+    strings: response.data.answer,
     autoStart: true,
     cursor : " ",
-    delay:10,
+    delay:1,
   });
-
-
 
 }
 
+
+function generateAnswers (event) {
+
+event.preventDefault();
+    
+let instructionsInput = document.querySelector ("#user-instructions");
+let apiKey = "2tb5293558cf27710004o841aef8f671";
+let context = "you are looking for financial guidance, ways to make money, anything that involves money";
+let prompt = `user instructions: generate answers about ${instructionsInput.value} into a short paragraph, leave margin top before starting the sentence it must be bold, separate each line with <br/>`;
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+console.log ("Generate answers");
+console.log (`(Prompt:   ${prompt}`);
+console.log (`Context:   ${context}`);
+
+axios.get(apiUrl).then(displayAnswers);
+
+}
+
+
 let answerGeneratorElement = document.querySelector ("#advices-generator");
 answerGeneratorElement.addEventListener ("submit", generateAnswers);
-
